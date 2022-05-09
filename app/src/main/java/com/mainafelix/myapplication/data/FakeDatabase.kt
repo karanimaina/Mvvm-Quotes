19a78ca2a8569726590046648cc6fc454fc4ffcc
felixmaina <karani.maina2010@gmail.com>
2022-05-09 12:25:34 +0300

    // todo generate a companion object that creates a single instance  of the class

---
+++ app/src/main/java/com/mainafelix/myapplication/data/FakeDatabase.kt
@@ -0,0 +1,10 @@
+package com.mainafelix.myapplication.data
+
+// TODO: generate a single ton pattern for the database and it will have a private constructor
+class FakeDatabase private constructor(){
+    // todo generate a companion object that creates a single instance  of the class
+    companion object{
+
+    }
+
+}
