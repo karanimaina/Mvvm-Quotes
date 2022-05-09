3f20d477a40bdfe94bc9beda59e212af8c85c639
felixmaina <karani.maina2010@gmail.com>
2022-05-09 12:18:16 +0300

    set an initializable that sets the mutable live data to the quote List

--- app/src/main/java/com/mainafelix/myapplication/data/FakeQuoteDao.kt
+++ app/src/main/java/com/mainafelix/myapplication/data/FakeQuoteDao.kt
@@ -4,5 +4,18 @@ import androidx.lifecycle.MutableLiveData
 
 class FakeQuoteDao {
     private val quoteList = mutableListOf<Quote>()
+//TODO "create
+// a variable quote that creates a mutable live data to help change the value of
+//the Live data from fakeDao class  and the data in this mutable data is of type List Quote
+
     private val quotes = MutableLiveData<List<Quote>>()
+
+    init {
+        //Todo assign the mutable live data to the quotes List
+        quotes.value=  quoteList
+    }
+    fun addQuote (quote :Quote){
+       quoteList.add(quote)
+        quotes.value = quoteList
+    }
 }
