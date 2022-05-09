06ef4c4e8b2109ca62d2514821fdcc11cc7583f3
felixmaina <karani.maina2010@gmail.com>
2022-05-09 12:58:16 +0300

    get Quottes from the from the fake quote whose data has been casted to live data

---
+++ app/src/main/java/com/mainafelix/myapplication/data/QuoteRepository.kt
@@ -0,0 +1,25 @@
+package com.mainafelix.myapplication.data;
+ class QuoteRepository private constructor(private val quoteDao: FakeQuoteDao ) {
+    // they are the source of data for the view model
+
+     fun addQuote(quote: Quote){
+         quoteDao.addQuote(quote)
+     }
+     fun getQoute () = quoteDao.getQuotes()
+    companion object{
+        // makes the right ofthis property visible to other threads
+        @Volatile
+        private var instance:QuoteRepository?= null
+        fun getInstance(quoteDao: FakeQuoteDao){
+            // use the elvis operrator to return instance if not nuull or else synchronise the
+            instance ?: synchronized(this){
+                //2 threads cannot be executing at the same time
+                //todo return fakedatabase instance if value is null
+                instance ?:QuoteRepository(quoteDao).also {
+                    //todo set the instance  to be equal to the database instance"
+
+                }
+            }
+        }
+
+    }}
